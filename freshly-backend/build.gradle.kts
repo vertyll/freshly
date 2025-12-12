9ce4945d0260b22c26459a05aa07bbf9c0f39086
Mikołaj Gawron <gawrmiko@gmail.com>
2025-12-12 22:06:53 +0100

    style: add comments to build gradle Kotlin DSL

--- freshly-backend/build.gradle.kts
+++ freshly-backend/build.gradle.kts
@@ -27,22 +27,37 @@ repositories {
 val mapstructVersion = "1.6.3"
 
 dependencies {
+	// Internal dependencies
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+
+	// MapStruct
     implementation("org.mapstruct:mapstruct:${mapstructVersion}")
+	annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+
+	// Ensure Lombok and MapStruct work together during annotation processing
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+	// Lombok
 	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	annotationProcessor("org.projectlombok:lombok")
-    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
-    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+	// Runtime
     runtimeOnly("org.springframework.boot:spring-boot-devtools")
+
+	// Docker Compose support
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+	// Testing dependencies
 	testImplementation("org.springframework.boot:spring-boot-starter-data-mongodb-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+
+	// Junit
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
