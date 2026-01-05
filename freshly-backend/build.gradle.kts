8c57a394b91198e09872ab088fbab95a6a1485a8
Mikołaj Gawron <gawrmiko@gmail.com>
2026-01-05 18:07:48 +0100

    chore(dependencies): bump versions

--- freshly-backend/build.gradle.kts
+++ freshly-backend/build.gradle.kts
@@ -148,16 +148,25 @@ subprojects {
 
     configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         java {
-            target("src/**/*.java")
-            googleJavaFormat(rootProject.libs.versions.google.java.format.get()).aosp().reflowLongStrings()
+            target("src/main/java/**/*.java", "src/test/java/**/*.java")
+            targetExclude("**/build/generated/**/*.java", "**/*Impl.java")
+
+            googleJavaFormat(rootProject.libs.versions.google.java.format.get()).aosp()
+
             removeUnusedImports()
-            endWithNewline()
+            importOrder("java", "javax", "org", "com", "com.vertyll")
+
             trimTrailingWhitespace()
+            endWithNewline()
+
+            toggleOffOn()
         }
 
-        kotlin {
-            target("**/*.gradle.kts")
+        kotlinGradle {
+            target("*.gradle.kts", "**/*.gradle.kts")
             ktlint(rootProject.libs.versions.ktlint.get())
+            trimTrailingWhitespace()
+            endWithNewline()
         }
     }
 
